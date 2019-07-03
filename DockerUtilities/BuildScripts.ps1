8d79feff7f7f0444124b8f7c9a9fdbae70436148
Jesus Hernandez <jesus.hc@globant.com>
2019-07-03 11:18:57 -0500

    validate docker version

--- DockerUtilities/BuildScripts.ps1
+++ DockerUtilities/BuildScripts.ps1
@@ -3,6 +3,8 @@
    Purpose: Build Dynamo
 #>
 
+docker --version
+
 docker pull artifactory.dev.adskengineer.net/docker-local-v2/dynamo/buildtools2017sdk81
 
 docker run -m 4GB -d -t --mount type=bind,source=C:\\Jenkins\\workspace\\Dynamo\\Dynamo,target=c:\\WorkspaceDynamo --name slow-test dynamo
