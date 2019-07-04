29f422ea8ef95628624032402f5210dec4ad31fc
Jesus Hernandez <jesus.hc@globant.com>
2019-07-04 15:30:29 -0500

    Update ExecuteParallel.ps1

--- DockerUtilities/ExecuteParallel.ps1
+++ DockerUtilities/ExecuteParallel.ps1
@@ -17,15 +17,15 @@ $pathSlow = "C:\\Jenkins\\workspace\\Dynamo\\Dynamo\\DYN-1822\\src\\Tools\\Trans
         #{&$ScriptToRun -path $pathFast}
         #{&$ScriptToRun -path $pathFast}
 
-        #{&".\ParallelTest.ps1 -path " + $pathFast}
-        #{&".\ParallelTest.ps1 -path " + $pathFast}
+        {&".\ParallelTest.ps1 -path " + $pathFast}
+        {&".\ParallelTest.ps1 -path " + $pathFast}
 
 
         #InlineScript { '$ScriptToRun -path $pathFast' }
         #InlineScript { '$ScriptToRun -path $pathFast' }
 
 
-        Start-Process -FilePath Powershell.exe -ArgumentList $ScriptToRun + " -path " + $pathFast
+        #Start-Process -FilePath Powershell.exe -ArgumentList $ScriptToRun + " -path " + $pathFast
     }
 
 }
