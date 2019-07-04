9d6acf71dc5215bb0d529eeee0c34ae180585c2b
Jesus Hernandez <jesus.hc@globant.com>
2019-07-04 11:44:13 -0500

    Remove ps1 that is not in use

--- DockerUtilities/ExecuteParallel.ps1
+++ DockerUtilities/ExecuteParallel.ps1
@@ -5,12 +5,11 @@
 
 workflow _Wkf_StartCommands {
 
-$ScriptToRun= "\ParallelTest.ps1"
+$ScriptToRun= ".\ParallelTest.ps1"
 
 $pathFast = "C:\\Jenkins\\workspace\\Dynamo\\Dynamo\\DYN-1822\\src\\Tools\\TransformXMLToCSVTool\\Result\\textFileWithFiltersFastTests.txt"
 
 $pathSlow = "C:\\Jenkins\\workspace\\Dynamo\\Dynamo\\DYN-1822\\src\\Tools\\TransformXMLToCSVTool\\Result\\textFileWithFiltersSlowTests.txt"
-
 
     parallel {
         {&$ScriptToRun -path $pathFast}
--- DockerUtilities/test.ps1
+++
@@ -1,6 +0,0 @@
-<#
-Date: 03/12/2019
-Purpose: Invoke testing configuration within the docker container
-#>
-
-docker exec localBuildContainer powershell -command "c:\libGCopy\DockerUtilities\runTestsInContainer.ps1"
