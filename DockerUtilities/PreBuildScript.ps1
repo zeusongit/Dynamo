280f7fb9930def5a166bdd4940c0277db790f286
Jesus Hernandez <jesus.hc@globant.com>
2019-07-04 17:47:55 -0500

    Increase container attributes

--- DockerUtilities/PreBuildScript.ps1
+++ DockerUtilities/PreBuildScript.ps1
@@ -5,4 +5,4 @@
 
 docker pull artifactory.dev.adskengineer.net/docker-local-v2/dynamo/buildtools2017sdk81
 
-docker run -m 4GB -d -t --mount type=bind,source=C:\Jenkins\workspace\Dynamo\Dynamo,target=c:\WorkspaceDynamo --name build-test artifactory.dev.adskengineer.net/docker-local-v2/dynamo/buildtools2017sdk81
+docker run -m 8GB --cpus=4 -d -t --mount type=bind,source=C:\Jenkins\workspace\Dynamo\Dynamo,target=c:\WorkspaceDynamo --name build-test artifactory.dev.adskengineer.net/docker-local-v2/dynamo/buildtools2017sdk81
