f3ec999f8dc1791e7f4518164a33afc6658c6ca2
alfredo-pozo <47230272+alfredo-pozo@users.noreply.github.com>
2019-08-01 15:52:25 -0700

    Removed CPU from docker run

--- DockerUtilities/PreBuildScript.ps1
+++ DockerUtilities/PreBuildScript.ps1
@@ -51,7 +51,7 @@ try
 	#Docker configuration
 	docker pull artifactory.dev.adskengineer.net/docker-local-v2/dynamo/buildtools2017sdk81
 
-	docker run -m 8GB --cpus=4 -d -t --mount type=bind,source=C:\Jenkins\workspace\Dynamo\Dynamo,target=c:\WorkspaceDynamo --name build-test artifactory.dev.adskengineer.net/docker-local-v2/dynamo/buildtools2017sdk81
+	docker run -m 8GB -d -t --mount type=bind,source=C:\Jenkins\workspace\Dynamo\Dynamo,target=c:\WorkspaceDynamo --name build-test artifactory.dev.adskengineer.net/docker-local-v2/dynamo/buildtools2017sdk81
 }
 catch
 {
