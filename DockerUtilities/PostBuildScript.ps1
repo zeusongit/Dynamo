fd840a002e63703404eec2adaae9b79b2617667c
Alfredo Pozo <alfredo.pozo@globant.com>
2019-09-01 14:48:38 -0500

    Another change to test with program files

--- DockerUtilities/PostBuildScript.ps1
+++ DockerUtilities/PostBuildScript.ps1
@@ -17,6 +17,13 @@ catch
 finally
 {
 	docker system prune -f
-	Restart-Service *docker*
+
+	$processes = Get-Process "*docker desktop*"
+	if ($processes.Count -gt 0)
+	{
+		$processes[0].Kill()
+		$processes[0].WaitForExit()
+	}
+	Start-Process "$env:ProgramFiles\Docker\Docker\Docker Desktop.exe"
 }
 
